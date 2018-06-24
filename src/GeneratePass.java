47366eb2754ba186d48556fda02ea6bcf22f9ba5
Bhaskar <bhaskarghosh16@icloud.com>
2018-06-24 20:34:54 +0530

    First commit

---
+++ src/GeneratePass.java
@@ -0,0 +1,14 @@
+import com.passcode.mains.Passcode;
+
+public class GeneratePass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Passcode passcode = new Passcode();
+		passcode.setGameName("Witcher3");
+		passcode.setYear(2018);
+		System.out.println(passcode);
+		
+	}
+
+}
